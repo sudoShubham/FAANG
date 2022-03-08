7f26337c8a2ed6e798115bc54d4381d214484e64
Shubham Pagare <shubhamp2017@gmail.com>
2022-03-08 21:42:29 +0530

    completed js fundamentals -2

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -80,18 +80,138 @@ console.log(res);
 
 //Arrays
 
+/*
 const ageCalculator = (birthYear) => 2022 - birthYear;
 
 const friends = ["Ram", "Sham", "Gopi", "Chandu"];
 const birthYears = new Array(2000, 1999, 1998, 1975);
 
-// console.log(friends.length);
-// console.log(friends[friends.length - 1]);
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
 
-// const myData = [friends[0], 2022 - 1999, "Engineer", friends];
-// console.log(myData);
-// console.log(typeof myData);
+const myData = [friends[0], 2022 - 1999, "Engineer", friends];
+console.log(myData);
+console.log(typeof myData);
 console.log(birthYears[0]);
 const age = new Array(ageCalculator(birthYears[0]));
 
 console.log(age);
+*/
+
+//Array Methods
+
+const friends = ["Ram", "Sham", "Gopi", "Chandu"];
+
+/*
+//Add elements
+const newLength = friends.push("Shubham");
+// console.table(friends);
+console.log(friends);
+console.log(newLength);
+friends.unshift("Golu");
+console.log(friends);
+
+//Remove element
+const deletedItem = friends.pop();
+console.log(deletedItem);
+const removeFirst = friends.shift();
+console.log(removeFirst);
+
+console.log(friends.indexOf("Gopi"));
+console.log(friends.includes("Sham"));
+*/
+
+/*
+//Challenge 2
+const calcTip = function (bill) {
+  if (bill <= 300 && bill >= 50) {
+    return 0.15 * bill;
+  } else {
+    return 0.2 * bill;
+  }
+};
+
+const bills = [125, 555, 44];
+const tips = [];
+const total = [];
+for (let x = 0; x < bills.length; x++) {
+  tips.push(calcTip(bills[x]));
+}
+
+for (let x = 0; x < bills.length; x++) {
+  total[x] = bills[x] + tips[x];
+}
+
+console.log(bills);
+console.log(tips);
+console.log(total);
+*/
+
+//Objects
+const data = {
+  name: "Shubham",
+  birthYear: 1999,
+  job: "Performance Engineer",
+  skills: ["JS", "C++", "DSA"],
+  calcAge: function (year) {
+    return 2022 - year;
+  },
+  calcAgeNew: function () {
+    this.age = 2022 - this.birthYear;
+    return 2022 - this.birthYear;
+  },
+  getSummary: function () {
+    return `Hi, my name is ${this.name}, My birth year is ${
+      this.birthYear
+    } and I am ${this.calcAgeNew()} years old.`;
+  },
+};
+
+/*
+console.log(data);
+console.log(data.name);
+console.log(data["name"]);
+console.log(data["na" + "me"]); //We can't do this in . operator
+
+const userInput = prompt("Enter your choice: name,age,job,skills");
+console.log(data.userInput); //Gives undefine output
+console.log(data[userInput]); //Works good
+*/
+/*
+data.location = "Nashik";
+data["email"] = "shubhamp2017@gmail.com";
+console.log(data);
+
+console.log(data.calcAge(data.birthYear));
+console.log(data["calcAge"](data.birthYear));
+console.log(data.calcAgeNew());
+console.log(data.getSummary());
+*/
+
+//Challege 3
+const john = {
+  name: "John",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+const mark = {
+  name: "Mark",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+
+const res =
+  john.calcBMI() > mark.calcBMI()
+    ? `John's bmi (${john.bmi}) is greater than mark's bmi(${mark.bmi}).`
+    : `John's bmi (${john.bmi}) is less than mark's bmi(${mark.bmi}).`;
+
+console.log(res);
