ffe1ecd3bc96e794b79514da9511bf1d8262b89b
Shubham Pagare <42333042+sudoShubham@users.noreply.github.com>
2022-05-09 21:30:20 +0530

    ejs implementation done

--- NodeJS/FIRST-NODE-PROJECT/app.js
+++ NodeJS/FIRST-NODE-PROJECT/app.js
@@ -10,7 +10,7 @@ const rootDir = require("./util/path");
 
 const app = express();
 
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: true }));
